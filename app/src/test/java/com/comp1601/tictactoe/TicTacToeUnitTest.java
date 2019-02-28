ced53609c9c4eab4ccbf7f60939c2634365d0bc3
Yiwei <zyw870315@gmail.com>
2019-02-28 05:51:24 -0500

    add buttons

--- app/src/test/java/com/comp1601/tictactoe/TicTacToeUnitTest.java
+++ app/src/test/java/com/comp1601/tictactoe/TicTacToeUnitTest.java
@@ -1,13 +1,11 @@
 package com.comp1601.tictactoe;
-
-import junit.framework.TestCase;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 import org.junit.rules.Timeout;
 import com.comp1601.tictactoe.TicTacToeGame;
 
+import static com.comp1601.tictactoe.TicTacToeGame.EMPTY_PIECE;
 import static junit.framework.TestCase.*;
 
 
@@ -24,7 +22,7 @@ public class TicTacToeUnitTest {
     public TestName name = new TestName();
 
     @Test
-    public void play_isCorrect()throws Exception {
+    public void play_isCorrect()throws Error {
         System.out.println("==============================");
         System.out.println("Test method name: {"+name.getMethodName()+"}");
         TicTacToeGame simpleGame =new TicTacToeGame();
@@ -33,7 +31,7 @@ public class TicTacToeUnitTest {
     }
 
     @Test
-    public void playOccupied_isCorrect()throws Exception {
+    public void playOccupied_isCorrect()throws Error {
         System.out.println("==============================");
         System.out.println("Test method name: {"+name.getMethodName()+"}");
         TicTacToeGame simpleGame =new TicTacToeGame();
@@ -43,7 +41,7 @@ public class TicTacToeUnitTest {
     }
 
     @Test
-    public void horizontalResult_isCorrect()throws Exception {
+    public void horizontalResult_isCorrect()throws Error {
         System.out.println("==============================");
         System.out.println("Test method name: {"+name.getMethodName()+"}");
         TicTacToeGame simpleGame =new TicTacToeGame();
@@ -56,7 +54,7 @@ public class TicTacToeUnitTest {
     }
 
     @Test
-    public void verticalResult_isCorrect()throws Exception {
+    public void verticalResult_isCorrect()throws Error {
 
         System.out.println("==============================");
         System.out.println("Test method name: {"+name.getMethodName()+"}");
@@ -70,7 +68,7 @@ public class TicTacToeUnitTest {
     }
 
     @Test
-    public void diagonalResult_isCorrect()throws Exception {
+    public void diagonalResult_isCorrect()throws Error {
          TestName name = new TestName();
         System.out.println("==============================");
         System.out.println("Test method name: {"+name.getMethodName()+"}");
@@ -79,14 +77,65 @@ public class TicTacToeUnitTest {
         simpleGame.play(TicTacToeGame.X_PIECE,4);
         simpleGame.play(TicTacToeGame.X_PIECE,0);
         //simpleGame.checkGameEnd();
-        //play a new piece , expected to on going
+        //play a new piece , expected to on going  # negative test
         System.out.println("Status ret :"+simpleGame.getStatus());
         assertNotSame(GameStatus.HUMAN_WIN, simpleGame.getStatus());
-        //play a new piece , expected to let human win
+        //play a new piece , expected to let human win  #positive test
         simpleGame.play(TicTacToeGame.X_PIECE,6);
         //simpleGame.checkGameEnd();
         System.out.println("Status ret :"+simpleGame.getStatus());
         assertEquals(GameStatus.HUMAN_WIN, simpleGame.getStatus());
+    }
+
+
+
+    @Test
+    public void drawGame_isCorrect()throws Error {
+        TestName name = new TestName();
+        System.out.println("==============================");
+        System.out.println("Test method name: {"+name.getMethodName()+"}");
+        TicTacToeGame simpleGame =new TicTacToeGame();
+        simpleGame.play(TicTacToeGame.X_PIECE,0);
+        simpleGame.play(TicTacToeGame.O_PIECE,1);
+        simpleGame.play(TicTacToeGame.X_PIECE,2);
+        simpleGame.play(TicTacToeGame.O_PIECE,4);
+        simpleGame.play(TicTacToeGame.X_PIECE,5);
+        simpleGame.play(TicTacToeGame.O_PIECE,3);
+        simpleGame.play(TicTacToeGame.X_PIECE,7);
+        simpleGame.play(TicTacToeGame.O_PIECE,8);
+        simpleGame.play(TicTacToeGame.X_PIECE,6);
+        //play a new piece , expected to on going
+        System.out.println("Status ret :"+simpleGame.getStatus());
+        assertEquals(GameStatus.DRAW_GAME, simpleGame.getStatus());
+    }
+
+
+    @Test
+    public void reset_isCorrect()throws Error {
+
+        TestName name = new TestName();
+        System.out.println("==============================");
+        System.out.println("Test method name: {"+name.getMethodName()+"}");
+        TicTacToeGame simpleGame =new TicTacToeGame();
+        simpleGame.humanPlay(4);
+        simpleGame.juniorAIPlay();
+        simpleGame.humanPlay(2);
+        simpleGame.juniorAIPlay();
+        simpleGame.humanPlay(8);
+        simpleGame.juniorAIPlay();
+        simpleGame.humanPlay(5);
+        System.out.println("Status ret :"+simpleGame.getStatus());
+        assertEquals(Player.PLAYER_SYMBOL,simpleGame.getFirstTurnPlayer());
+        //reset the game
+        simpleGame.resetGame();
+        System.out.println("Status ret :"+simpleGame.getStatus());
+        for (int k=0;k<3;k++){
+            assertEquals(EMPTY_PIECE,simpleGame.getChessBoard()[k][0]);  //the left slot of a single line
+            assertEquals(EMPTY_PIECE,simpleGame.getChessBoard()[k][1]); //the left slot of a single line
+            assertEquals(EMPTY_PIECE,simpleGame.getChessBoard()[k][2]);  //the left slot of a single line
+        }
+
+        assertEquals(Player.AI_SYMBOL,simpleGame.getFirstTurnPlayer());
     }
 
 }
